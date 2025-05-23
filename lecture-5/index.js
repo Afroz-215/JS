62798a3eb996d40687e45108e520480bfcaeeae5
Afroz-215 <afrozj215@gmail.com>
2025-05-23 23:32:40 +0530

    js

---
+++ lecture-5/index.js
@@ -0,0 +1,90 @@
+// // let arr =[1,2,3,4,5,6,7,3]
+// // console.log(arr.includes(3))
+// // console.log(arr.indexOf(100))
+
+
+
+// // const people = [
+// //   { name: "John", age: 25 },
+// //   { name: "Alice", age: 30 },
+// //   { name: "Bob", age: 20 }
+// // ];
+
+// // people.sort((a, b) => a.age - b.age); // Sort by age (ascending)
+// // console.log(people);
+// // Output: [{ name: "Bob", age: 20 }, { name: "John", age: 25 }, { name: "Alice", age: 30 }]
+
+// let arr1 =[1,2,3,4,4,5,6,7,3]
+// let arr2 =[1,43,12,4,2,5]
+
+// let a = arr1.concat(arr2)
+// console.log(a)
+
+// // let merge = [...arr1, ...arr2];
+// // console.log(merge);
+
+// let merge = []
+// for(let i of arr1){
+// merge.push(i)
+// }
+// for(let i of arr2){
+// merge.push(i)
+// }
+// console.log(merge)
+
+// console.log(Math.floor(Math.random()*10))
+
+// let luckyNo = Math.floor(Math.random() * 10)
+// console.log(luckyNo)
+// let val = Number(prompt("enter a no."))
+
+// if (luckyNo == val) console.log("you are winner ")
+// else console.log("try again")
+
+let arr = [-3, 0, 3, 5, 5, 2, 0, 1];
+let k = 3;
+
+function maxSubarray(arr, k) {
+    let result = [];
+    let i = 0;
+    let j = k - 1;
+
+    while (j < arr.length) {
+        let max = arr[i];
+        for (let m = i + 1; m <= j; m++) {  
+            if (arr[m] > max) {
+                max = arr[m];
+            }
+        }
+
+        result.push(max);
+        i++;
+        j++;
+    }
+
+    return result;
+}
+
+console.log(maxSubarray(arr, k));
+
+
+function maxSumSubarray(arr, k) {
+    let maxSum = -Infinity;
+    let windowSum = 0;
+
+    let i = 0;
+    for (let j = 0; j < arr.length; j++) {
+        windowSum += arr[j];
+
+        if (j - i + 1 === k) {
+            maxSum = Math.max(maxSum, windowSum);
+            windowSum -= arr[i];
+            i++;
+        }
+    }
+
+    return maxSum;
+}
+
+
+console.log(maxSumSubarray(arr, k));
