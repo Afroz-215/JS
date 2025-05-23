0cca396be1054f438dd371da0500daa57cb1d230
Afroz-215 <afrozj215@gmail.com>
2025-05-23 11:27:54 +0530

    js

---
+++ lecture-2/index.js
@@ -0,0 +1,107 @@
+// console.log(5 + 5)
+// console.log(5 + "5")
+// console.log(5 - "5")
+// // && 
+// console.log(true && false && true && false)
+// console.log(true  false  true  false)
+// console.log(true && false)
+
+
+
+// // 5 => 101
+// // 4 => 100
+// console.log(5 & 4) // 100 => 4
+
+// // These values are treated as false in boolean contexts:
+
+// // false
+// // 0
+// // "" (empty string)
+// // null
+// // undefined
+// // NaN
+// // Everything else is truthy
+
+// let isBool
+// if (isBool) console.log("hello")
+// else console.log("hii")
+
+// // The bitwise AND operator (&) compares two numbers bit by bit and returns a number where each bit is 1 only if both corresponding bits in the operands are 1
+// // 0101  (binary for 5)
+// // & 0011  (binary for 3)
+// // --------
+// // 0001  (result)
+
+// // The result is 0001 in binary, which is 1 in decimal
+
+
+// if (5 & 3) console.log("hello")
+// else console.log("hii")
+
+// // let data =prompt("enter your name")
+// // console.log(data)
+
+// // even odd no 
+// let num = Number(prompt("enter a number"))
+// if (num % 2 == 0) console.log("even number")
+// else console.log("odd number")
+
+// isBool = true
+// isBool ? console.log("hello") : console.log("hii")
+
+
+// // if else if else 
+
+// let n = 10;
+
+// if (n > 0) {
+//     console.log("The number is positive");
+// } else if (n < 0) {
+//     console.log("The number is negative");
+// } else {
+//     console.log("The number is zero");
+// }
+
+// let str = "123";
+// let val = +str;
+
+// // The + operator in front of the 
+// string "123" forces JavaScript to convert 
+// it to the number 123.
+
+// // The result is now a number, not a string.
+
+// console.log(val);  // Output: 123
+// console.log(typeof val);
+
+// console.log(5+"hello")
+// console.log(typeof(5+"hello"))
+// console.log(5-"hello")
+
+// console.log(typeof(NaN))
+
+// let name1 ="aanya"
+// let name2 ="afroz"
+// let name3 ="anuradha"
+
+// // non-primitive datatype
+// // is collection of primitive datatype
+
+// const std ={
+//     name1:"anya",
+//     name2:"afroz",
+//     name3:"anuradha",
+//     age: 23
+// }
+// std.age =27
+// console.log(std.name2)
+// console.log(std.age)
+
+// console.log({} == {})
+
+// let a ={name:"ritik"}
+// let b= {name:"ritik"}
+// let c =a
+
+// console.log(a == b)
+// console.log(a == c)
---
+++ lecture-3/index.js
@@ -0,0 +1,118 @@
+// let arr =[1,2,3,4,"afroz",false]
+// console.log(arr[3])
+// console.log("hii")
+
+// const { func } = require("prop-types")
+
+//  let arr=[1,2,3,4,5,6,67,8]
+// let newA=[]
+
+// // let newArr=[4,5,6,67,8]
+// arr.push(100)
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>3){
+//         newA.push(arr[i])
+//     }  
+// }
+// console.log(newA);
+
+
+
+// function sum(){
+//     console.log("hello")
+// }
+// sum()
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(10,50)
+
+// let show = (a) => {
+//     console.log(a)
+// }
+// show(2)
+
+// for(let  i=0;i<10;i++){
+// console.log(i)
+// }
+
+// let arr = [1,2,3,4,5,6,7,8]
+// count = 0
+
+// // for(let i=arr.length-1;i>=0;i--){
+// //     console.log(arr[i])
+// // }
+
+// function sum(data){
+// //   console.log(data)
+//   for(let i of data){
+//     count+=i
+//   }
+// console.log(count)
+// }
+// sum(arr)
+// console.log([]==[]);
+
+// let arr =[ {id:1,name:"afroz"},{id:2,name:"aanya"}]
+
+// for(let i of arr){
+//     console.log(i.name)
+//     console.log(i.id)
+//     console.log(i)
+// }
+
+// let obj ={
+//     id:1,name:"afroz"
+
+// }
+// console.log(obj)
+
+// A function is a reusable block of code.
+
+// A method is a function that is attached to an object
+
+// let obj ={
+//     id:1,
+//     name:"rahul",
+//     lastName:"jain",
+//     fullName:function(){
+//         console.log(obj.name + " " +obj.lastName)
+//     }
+// }
+// obj.fullName()
+
+// let arr =[1,4,6,8,10]
+// arr.push(100);
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.unshift(10)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+ // let arr=[1,2,3,4,5,6]
+// let target = 3
+//  let data =arr.find ((a) =>{
+//     return a == target
+// })
+// console.log(data)
+
+// for(let i of arr){
+//     console.log(i);
+
+// }
+// for(let i in arr){
+//     console.log(i);
+
+// }
+
+
+
+// function sum(){
+//     return 5
+//     console.log("hiiii")
+// }
+// console.log(sum())
