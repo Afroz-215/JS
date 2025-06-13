c823664d1638c8062ee6d6d65cca4c7aa92bbeed
Afroz-215 <afrozj215@gmail.com>
2025-06-13 21:11:16 +0530

    promise

---
+++ lecture-19/index.js
@@ -0,0 +1,77 @@
+// let step1 = function(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res("choose picture ")
+//         },5000)
+//     })
+// }
+
+// let step2 = function(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res("apply filter")
+//         },5000)
+//     })
+// }
+
+// // 
+
+
+// step1().then((data)=>{
+//     console.log(data)
+//     return step2()
+// }).then((filter)=>{
+//     console.log(filter)
+// })
+
+//    let promise=  new Promise((res,rej)=>{
+//     // res()
+//     // rej("errr")
+//     res("hello")
+//    })
+//    promise.then((data)=>{
+//     console.log(data);
+//    }).catch((err)=>{
+//     console.log(err);
+//    })
+
+ let step1=   function(){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res("select a photo")
+        },5000)
+    })
+   }
+
+    
+   let step2=   function(){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res("filterrrrr")
+
+        },4000)
+    })
+   }
+ //callback hell => promise=> async await
+
+//    step1().then((data)=>{
+//     console.log(data);
+//        return step2()
+//    }).then((filterr)=>{
+//     console.log(filterr);
+//    })
+
+
+
+ async  function  call(){
+  let data=   await   step1()
+  console.log(data);
+
+    let data1=   await  step2()
+    console.log(data1);   
+}
+call()
+
+
+
+   
