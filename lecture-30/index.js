7fcaf52d36a8fad656ab233b665a7ca85855b10c
Afroz-215 <afrozj215@gmail.com>
2025-08-23 11:44:48 +0530

    methods of promise

---
+++ lecture-30/index.js
@@ -0,0 +1,28 @@
+// // let p1 =new Promise((res,rej)=>{
+// //     res()
+// // })
+// // let p2 =new Promise((res,rej)=>{
+// //     rej()
+// // })
+// // let p3 =new Promise((res,rej)=>{
+// //     res()
+// // })
+
+// let p1 = new Promise(res => setTimeout(() => res("One"), 300));
+// let p2 = new Promise(res => setTimeout(() => res("Two"), 200));
+// let p3 = new Promise(res => setTimeout(() => res("Three"), 100));
+
+// // Promise.all([p1,p2,p3]).then((values)=>{
+// //     console.log(values)
+// // })
+// // Promise.allSettled([p1,p2,p3]).then((data)=>{
+// //     console.log(data)
+// // })
+// // Promise.race([p1,p2,p3]).then((value)=>{
+// //     console.log(value)
+// // })
+// Promise.any([p1,p2,p3]).then((value)=>{
+//     console.log(value)
+// })
+
+console.log("first")
